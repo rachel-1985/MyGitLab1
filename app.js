49c89e4ad0a9878a83bc1d1aea6c9c0662a0b3b7
root <root@ip-172-31-27-182.us-west-2.compute.internal>
2022-09-12 06:22:43 +0000

    MY UPDATED DATA

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ angular.module('sortApp', [])
   
   // create the list of sushi rolls 
   $scope.sushi = [
-    { name: 'attende1', fish: 'Ricky', tastiness: 67 },
-    { name: 'finisher', fish: 'CR7', tastiness: 78 },
-    { name: 'runner', fish: 'Ronaldo', tastiness: 80 },
-    { name: 'cricketer', fish: 'messi', tastiness: 83 },
+    { name: 'attende1', fish: 'Ramji', tastiness: 67 },
+    { name: 'finisher', fish: 'Rachel', tastiness: 78 },
+    { name: 'runner', fish: 'Riddhi', tastiness: 80 },
+    { name: 'cricketer', fish: 'Ridhaan', tastiness: 83 },
     
   ];
   
